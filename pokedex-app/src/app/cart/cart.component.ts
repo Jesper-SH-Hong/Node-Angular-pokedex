de7618ba772e6d56cac655aaac3efc156323dcc1
seong <seonghyun91@gmail.com>
2022-10-12 00:13:06 -0700

    check scope for if statement

--- pokedex-app/src/app/cart/cart.component.ts
+++ pokedex-app/src/app/cart/cart.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
-//import from Cart Service
 import { CartService } from '../services/cart.service';
 
 @Component({
@@ -8,20 +6,16 @@ import { CartService } from '../services/cart.service';
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
+
+
 export class CartComponent implements OnInit {
-
   //define items property to save cart item list
-  // items = this.cartService.getItems();
   items: any
-
   totalPrice: any
 
   constructor(
     private cartService: CartService
   ) { }
-
-
-
 
   async ngOnInit(): Promise<void> {
     console.log('This is cart')
@@ -29,19 +23,14 @@ export class CartComponent implements OnInit {
     await this.cartService.getItems().then(value => this.items = value);
 
     this.totalPrice = this.cartService.getTotalPrice(this.items)
-
     console.log(3);
     console.log(this.items)
     console.log(4);
   }
 
-
-
   emptyCart(): void {
     this.cartService.clearCart()
     window.alert('Remove all the items in Cart')
-
   }
-
 
 }
--- pokedex-app/src/app/pokemon-list/pokemon-list.component.ts
+++ pokedex-app/src/app/pokemon-list/pokemon-list.component.ts
@@ -1,50 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
-
 import { CartService } from '../services/cart.service';
 import {HttpClient} from '@angular/common/http';
-
-
-//for search form and binding
 import { FormGroup, FormControl } from '@angular/forms';
-
-
-
-
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.css']
 })
+
 export class PokemonListComponent implements OnInit {
 
   pokemons: any[] = [];
-
-
 
   searchForm = new FormGroup({
     pokeName: new FormControl(''),
   });
 
 
-
   constructor(
     //injecting my services!
     private dataService: DataService,
     private cartService: CartService,
-
     private http: HttpClient
-
-
   ) { }
 
   ngOnInit(): void {
     this.dataService.getPokemons()
     .subscribe((response: any) => {
       // console.log(response);
-      
-
 
       response.results.forEach( (result: any) => {
         this.dataService.getMoreData(result.name)
@@ -54,50 +39,32 @@ export class PokemonListComponent implements OnInit {
 
         })
       })
-
       //.subscribe
       // 서버의 응답이 언제 도착하든지, 이 응답이 도착했을 때 subscribe가 서버에서 받은 응답을 콜백 함수로 전달. jquery의 res나 여기 response나 같은 거 ㅋㅋ
-
-
-
     });
   }
 
   addToCart(poke: any): void {
     this.cartService.addToCart(poke)
     window.alert('Your Pokemon has been added!')
-
   }
 
 
   searchWithPokeName() {
-
-    // 현 pokemon-grid 내용 지우기..
-    this.pokemons = [];
-
-    
-
-    // TODO: EventEmitter로 폼 내용 보내기
-    console.warn(this.searchForm.value.pokeName);
-
+    this.pokemons = [];    
+    // console.warn(this.searchForm.value.pokeName);
     this.dataService.getPokemons()
     .subscribe((response: any) => {    
-
-
       response.results.forEach( (result: any) => {        
-        if(result.name == this.searchForm.value.pokeName)
-        this.dataService.getMoreData(result.name)
-        .subscribe((uniqResponse: any) => {
-          this.pokemons.push(uniqResponse);
-          console.log(this.pokemons);
-
-        })
+        if(result.name == this.searchForm.value.pokeName){
+          this.dataService.getMoreData(result.name)
+          .subscribe((uniqResponse: any) => {
+            this.pokemons.push(uniqResponse);
+            console.log(this.pokemons);
+          })
+        }
       })
-
-      //.subscribe
-      // 서버의 응답이 언제 도착하든지, 이 응답이 도착했을 때 subscribe가 서버에서 받은 응답을 콜백 함수로 전달. jquery의 res나 여기 response나 같은 거 ㅋㅋ
-
-  });
+    });
   }
 
 }
